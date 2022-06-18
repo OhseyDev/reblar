0b07567c7430d6edff7010e6aef581dbbd912ed1
Olivia Thompson <me@ocolivia.dev>
2022-06-18 14:04:16 +0100

    Read every line in markdown document + print

--- src/md.rs
+++ src/md.rs
@@ -1,9 +1,19 @@
 use std::fs;
+use std::io::{BufRead, BufReader};
 
 pub fn build(source_file: &'static str) -> Result<(), String> {
-    let file = fs::File::open(source_file);
-    if file.is_err() {
-        return Err("Unknown file error".to_string());
+    let reader = {
+        let file = fs::File::open(source_file);
+        if file.is_err() {
+            return Err("Unknown file error".to_string());
+        }
+        let file = file.unwrap();
+        BufReader::new(file)
+    };
+    for (index, line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+        // TODO: handle parsing of Markdown document
+        println!("{}. {}", index + 1, line);
     }
     return Ok(());
 }
